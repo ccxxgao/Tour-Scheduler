b0fc6a8930f4fe2ddcbc4716befd7c493484c2f6
ccxxgao <ccxxgao@users.noreply.github.com>
2020-03-07 16:08:18 -0500

    scheduler.py

--- scheduling.py
+++ scheduling.py
@@ -14,27 +14,47 @@ def getAvailabilities(string):
     return avail 
 
 if __name__ == "__main__":
-    A = pd.read_csv('availabilities.csv')
+    fileName = input("Enter csv file name: ")
+    A = pd.read_csv(fileName)
+
+    # Preprocessing
     A['days_available'] = A["When are you available for tours?"].apply(lambda x: getAvailabilities(x))
-    A = A.drop(A.columns[[0,1,3,4,6,7]], axis=1)
+    cols = list(A)
+    for i in range(len(cols)):
+        print("Column", i, " ", cols[i])
+    a = [int(x) for x in input("Columns indices to drop: ").split()]
+    A = A.drop(A.columns[a], axis=1)
+    if 'Days_off' not in list(A):
+        A['Days_off'] = 0
     A['num_of_tours'] = 0
     A['Name'] = A['Name'].str.split(" ", n = 1, expand = True)
     A['gave_tour_this_week'] = 0
-    # A['gave_tour_last_week'] = 0
     temp = A['days_available'].apply(pd.Series)
     weekdays = ['M', 'T', 'W', 'Th', 'F']
     temp.columns = weekdays
     A = A.join(temp, how='outer')
     A = A.join(pd.DataFrame(np.zeros((len(A), len(A)), dtype=int)), how='outer')
-    A = A.drop(A.columns[2], axis=1)
+    A = A.drop(columns=['days_available'])
 
     # assign head guides four tours each
-    head_guides = [2, 5]
+    heads = input("Head Guides: ").split()
+    head_guides = []
+    for i in heads:
+        head_guides.append(A[A['Name'] == i].index.tolist()[0])
+    head_tours = input("Max number of tours to assign head guides: ")
+    if head_tours == "":
+        head_tours = 0
+    else:
+        head_tours = int(head_tours)
+
     # dict stores semester assignments 
     # week : [(g1, g2), (g1, g2)...]
     semester_assignments = {}
 
-    for week in range(0, 11):
+    w = int(input("Number of weeks: "))
+    num_guides = int(input("Guides per tour: "))
+
+    for week in range(0, w):
         A['gave_tour_this_week'] = 0
         semester_assignments[week] = []
         for day in weekdays:
@@ -50,23 +70,21 @@ if __name__ == "__main__":
             # Remove head guides if they have given 3 tours
             for h in head_guides:
                 if h in list(available_guides.index):
-                    if A.iloc[h, A.columns.get_loc('num_of_tours')] == 4:
+                    if A.iloc[h, A.columns.get_loc('num_of_tours')] == head_tours:
                         available_guides = available_guides.drop(h)
             assigned_guides = []
             curr_guide = -1
 
             # Select Guide 1
-            while True:
-                curr_guide = available_guides[available_guides.num_of_tours==available_guides.num_of_tours.min()]
-                curr_guide = curr_guide[curr_guide.gave_tour_this_week==curr_guide.gave_tour_this_week.min()].sample(n=1)
-                A.iloc[curr_guide.index[0], A.columns.get_loc('num_of_tours')] += 1
-                A.iloc[curr_guide.index[0], A.columns.get_loc('gave_tour_this_week')] = 1
-                assigned_guides.append(curr_guide.index[0])
-                available_guides = available_guides.drop(curr_guide.index[0])
-                break
+            curr_guide = available_guides[available_guides.num_of_tours==available_guides.num_of_tours.min()]
+            curr_guide = curr_guide[curr_guide.gave_tour_this_week==curr_guide.gave_tour_this_week.min()].sample(n=1)
+            A.iloc[curr_guide.index[0], A.columns.get_loc('num_of_tours')] += 1
+            A.iloc[curr_guide.index[0], A.columns.get_loc('gave_tour_this_week')] = 1
+            assigned_guides.append(curr_guide.index[0])
+            available_guides = available_guides.drop(curr_guide.index[0])
 
-            # Select guide 2
-            while True:
+            # Select Guide 2
+            if num_guides == 2:
                 # get the remaining available guides in a list
                 available_guides = available_guides[available_guides.num_of_tours==available_guides.num_of_tours.min()]
                 available_guides = available_guides[available_guides.gave_tour_this_week==available_guides.gave_tour_this_week.min()]
@@ -77,16 +95,21 @@ if __name__ == "__main__":
                 A.iloc[attempt, A.columns.get_loc('num_of_tours')] += 1
                 A.iloc[attempt, A.columns.get_loc('gave_tour_this_week')] = 1
                 assigned_guides.append(attempt)
-                break
                 
-            # add 1 to both guides' paired w/
-            A.iloc[assigned_guides[0], A.columns.get_loc(assigned_guides[1])] += 1
-            A.iloc[assigned_guides[1], A.columns.get_loc(assigned_guides[0])] += 1
-            guides = []
-            for g in assigned_guides:
-                guides.append(A.iloc[g, A.columns.get_loc('Name')])
-            semester_assignments[week].append(guides)             
-    
+                # add 1 to both guides' paired w/
+                A.iloc[assigned_guides[0], A.columns.get_loc(assigned_guides[1])] += 1
+                A.iloc[assigned_guides[1], A.columns.get_loc(assigned_guides[0])] += 1
+                guides = []
+                for g in assigned_guides:
+                    guides.append(A.iloc[g, A.columns.get_loc('Name')])
+                semester_assignments[week].append(guides)             
+
+            # For new guides
+            else:
+                guides = []
+                for g in assigned_guides:
+                    guides.append(A.iloc[g, A.columns.get_loc('Name')])
+                semester_assignments[week].append(guides)             
 
     for key, val in semester_assignments.items():
         print("Week ", key, ": ", val)
